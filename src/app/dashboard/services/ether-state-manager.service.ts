2ed8a703ad76d6db02dba0ed5cde50536ce625f4
Eden Ben Ovadia <edbenova@microsoft.com>
2021-12-12 16:49:33 +0200

    Changed to use only one api that returns all

--- src/app/dashboard/services/ether-state-manager.service.ts
+++ src/app/dashboard/services/ether-state-manager.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
-import { bufferCount, catchError, concatMap, from, map, Observable, of, switchMap, tap } from 'rxjs';
+import { concatMap, Observable, tap } from 'rxjs';
 
 import { EtherScanHttpService } from './ether-scan-http.service';
 import { WalletService } from './wallet.service';
 import { ComponentStore } from '@ngrx/component-store';
 import { Token, TokenState } from '../';
-import { combineLatest } from 'rxjs';
-import { ethers } from 'ethers';
 
-const batchSize = 3;
 const EmptyState: TokenState = {
   tokensEntities: new Map<string, Token>(),
   tokensList: [],
@@ -25,81 +22,17 @@ export class EtherStateManagerService extends ComponentStore<TokenState> {
     super(EmptyState);
   }
 
-  readonly loadTokens = this.effect((tokens$: Observable<string[]>) => {
-    return from(tokens$)
+  readonly loadTokens = this.effect(() => {
+    return this.wallet.getAccount()
     .pipe(
-      switchMap(tokens => from(tokens)),
-      bufferCount(batchSize),
-      concatMap((batch) => {
-          const calls: Observable<Token>[] = [];
-          batch.forEach(tokenAddressInBatch => calls.push(this.etherScanHttpService.getToken(tokenAddressInBatch)));
-          return combineLatest(calls)
+      concatMap((address) => {
+        return this.etherScanHttpService.getBalanceForAddress(address);
       }),
       tap((tokens) => {
-        //upsertTokens to have partial data until balances are back from BE
-        this.upsertTokens(tokens)
-
-        //load user holdings for each token
-        this.loadHoldings(tokens);
-
-        //load price in dollars for each token
-        this.loadTokenPriceDollars(tokens);    
+        this.addTokens(tokens)
       }),
     );
   });
-
-  private readonly loadHoldings = this.effect((tokens$: Observable<Token[]>) => {
-      return combineLatest([this.wallet.getAccount(), tokens$])
-      .pipe(
-        concatMap(([activeAddress, tokens]) => {
-          const networkCalls: Observable<Token>[] = [];
-
-          // create array of http networkCalls
-          tokens.forEach(token => 
-            networkCalls.push(
-              this.etherScanHttpService
-              .getUserBalanceForToken(token.address, activeAddress)
-                .pipe(
-                  map((balance) => this.assignBalance(token, balance)),
-                  tap(v => console.log(v)),
-                  catchError((err) => {
-                    console.log('error in loadHoldings: ' + err);
-                    return of(err)
-                  }),
-                )
-            )
-          );
-
-          return combineLatest(networkCalls)
-        }),
-        tap((tokensWithBalances) => {
-          console.log(tokensWithBalances)
-          this.upsertTokens(tokensWithBalances);
-        }),
-        catchError((err) => {
-          console.log('error in loadHoldings: ' + err);
-          return of(err)
-        }),
-      );
-  });
-
-  private readonly loadTokenPriceDollars = this.effect((tokens$: Observable<Token[]>) => {
-    return tokens$
-    .pipe(
-      concatMap((tokens) => {
-        // create array of http networkCalls
-        return this.etherScanHttpService.getTokensPriceDollars(tokens.map(token => token.symbol), 'USD')
-        .pipe(
-          map((pricesAndSymbols: any) => {
-            return tokens.map(token => this.assignPrices(token, pricesAndSymbols[token.symbol]?.USD))
-          })
-        )
-      }),
-      tap((tokensWithBalances) => {
-        this.upsertTokens(tokensWithBalances);
-      })
-    );
-});
 
   readonly upsertTokens = this.updater((state, tokens: Token[]) => {
     const { tokensEntities } = state;
@@ -119,28 +52,23 @@ export class EtherStateManagerService extends ComponentStore<TokenState> {
     }
   });
 
+  readonly addTokens = this.updater((state, tokens: Token[]) => {
+    const tokensMap = new Map<string, Token>()
+    tokens.forEach(token => {
+      const key = token.symbol;
+      tokensMap.set(key, token);
+    });
+
+    return <TokenState> {
+      ...state,
+      tokensEntities: tokensMap,
+    }
+  });
+
   readonly tokens$: Observable<Token[]> = this.select(state => {
     const tokensList: Token[] = [];
     state.tokensEntities.forEach(token => tokensList.push(token));
 
     return tokensList;
   });
-  
-  private assignBalance(token: Token, balance: string): Token {
-    let formattedBalance = ethers.utils.formatEther(balance);
-    const [left, right] = formattedBalance.split('.');
-    formattedBalance = left + '.' + right.substring(0, 3);
-
-    return Object.assign(token, <Token> { 
-      quantity: formattedBalance,
-    });
-  }
-  
-  private assignPrices(token: Token, priceInDollars: string): Token {
-    const value: number = Number.parseFloat(priceInDollars);
-
-    return Object.assign(token, { 
-      priceInDollars: value,
-    });
-  }
 } 
